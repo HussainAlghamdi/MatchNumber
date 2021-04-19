74c248fd54ec1d88aae2b290700326f53a36e7d7
Hussain <H@gmail.com>
2021-04-19 05:10:29 +0300

    first commit

---
+++ MatchNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MatchNumber
+{
+    class Program
+    {
+        static bool match(string source)
+        {
+            Stack<int> stack = new Stack<int>();
+            foreach (char ch in source)
+            {
+                if (stack.Count == 0 || ch != stack.Peek())
+                {
+                    stack.Push(ch);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+                
+            }
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(match("89339998"));
+        }
+    }
+}
